4c0a27be6a400e6fe84e70f92e8648f7f155e907
Nayantara Das <nayantara.das@gmail.com>
2022-06-15 18:16:35 -0500

    Basic Firebase config with AppDelegate

--- studyCoach/studyCoachApp.swift
+++ studyCoach/studyCoachApp.swift
@@ -7,8 +7,21 @@
 
 import SwiftUI
 
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 @main
 struct studyCoachApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @Environment(\.scenePhase) private var scenePhase
     @StateObject var classData: ClassData = ClassData()
     @StateObject var tData: taskData = taskData()
